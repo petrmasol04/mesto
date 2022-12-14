ca9dd9c1ceb6f3e2fb98f590f53d66408afae34a
Petr Masol <petyamasol@gmail.com>
2022-12-14 16:45:46 +0300

    feat: переместил массив в отдельный файл

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,4 @@
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+import initialCards from './arrays.js';
 
 const popupProfile = document.querySelector('.popup_profile');
 const popupBtnCloseProfile = popupProfile.querySelector('.popup__btn-close_profile');
