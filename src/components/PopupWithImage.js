3e2185a5ef484c3f28479a9472151a515d1c6ed4
Petr Masol <petyamasol@gmail.com>
2023-02-13 22:06:53 +0300

    feat: Изменить "можно лучше"

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,11 +3,11 @@ import Popup from './Popup.js';
 export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
+        this._popupLookImg = this._popup.querySelector('.popup__image');
+        this._popupCaption = this._popup.querySelector('.popup__caption');
     }
 
     open(name, link) {
-        this._popupLookImg = this._popup.querySelector('.popup__image');
-        this._popupCaption = this._popup.querySelector('.popup__caption');
         this._popupLookImg.alt = name;
         this._popupLookImg.src = link;
         this._popupCaption.textContent = name;
